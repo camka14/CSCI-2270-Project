58a9961f9b602ac46a115d145f8357014786d65f
camka14 <camka14@gmail.com>
2019-04-14 15:00:23 -0600

    Initial Header Update

---
+++ Header.hpp
@@ -0,0 +1,78 @@
+#include <string>
+// Add #includes
+
+
+struct Word
+{
+	string word;
+	float TF;
+	float IDF;
+	int countTF;
+	int countIDF;
+};
+
+struct Sent
+{
+	string sent;
+	float score;
+	Sent *next;
+};
+
+class Stack
+{
+
+	Stack();
+	~Stack();
+	bool isEmpty();
+	void push(float num);
+	void pop();
+	Sent* peek();
+	Sent* getStackHead() { return stackHead; } // no need to implement this
+private:
+	Operand* stackHead; // pointer to the top of the stac
+};
+
+class Heap
+{
+	public:
+    Heap(int queueSize);
+    ~Heap();
+    void enqueue (std::string _groupName, int _groupSize, int _cookingTime);
+    void dequeue();
+    GroupNode peek();
+    bool isFull();
+    bool isEmpty();
+
+    private:
+    void repairUpward(int nodeIndex);
+    void repairDownward(int nodeIndex);
+
+    GroupNode* priorityQueue;
+    int currentQueueSize;
+    int maxQueueSize;
+};
+
+class Hash
+{
+	public:
+    Hash(int hashTableSize);
+    ~Hash();
+    void addWord(std::string word);
+    bool isInTable(std::string word);
+    void incrementCount(std::string word);
+    void printTopN(int n);
+    int getNumCollisions();
+    int getNumItems();
+    int getTotalNumWords();
+
+  private:
+    /* member functions */
+    unsigned int getHash(std::string word);
+    wordItem* searchTable(std::string word);
+
+    /* instance variables */
+    wordItem** hashTable;
+    int hashTableSize;
+    int numItems;
+    int numCollisions;
+};
