4b74a9c467dd92e52ccf8875a52e6442b3d81965
camka14 <camka14@gmail.com>
2019-04-16 14:38:49 -0600

    Adding Refrence Files

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
 #include "Header.hpp"
+// #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-	
+	ifstream file;
+	Stack sentStack;
+	Hash wordHash;
+	Heap sentHeap;
+	string stringLine;
+	int count=0;
+
+	file.open(argv[1]);
+
+	string word;
+	string sentence;
+
+	char* tok = strtok
 }
