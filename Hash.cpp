2db823f6b6c04bb8489a4759e3aa43458311bd15
Rcmoellenhoff <49414267+Rcmoellenhoff@users.noreply.github.com>
2019-04-18 15:25:54 -0600

    Update for Hash.cpp
    Just good to check out.
    Not sure about the increment counts, I believe I will need the TDF stuff for incrementCount() function among others

--- Hash.cpp
+++ Hash.cpp
@@ -10,24 +10,63 @@ Hash(int hashTableSize){
 }
 ~Hash();
 void addWord(string word) {
-	if(!searchTable(word))
+	if(!isInTable(word))
 	{
 		int index = getHash(word);
 		if(!hashTable[index])
-			hashTable[index] = 
+			hashTable[index] = createNode(word,nullptr);
+		else {
+			Word *temp = hashTable[index];
+			while(temp->next)
+				temp = temp->next;
+			temp->next = createNode(word,nullptr);
+		}
+	}
+	else {
+		cout << "Already exists: " << word << endl;
 	}
 }
-bool isInTable(string word);
-void incrementCount(string word);
+bool isInTable(string word) {
+	int index = getHash(word);
+
+	node *temp = hashTable[index];
+	while(temp) {
+		if(temp->word == word)
+			return true;
+		else 
+			temp = temp->next;
+	}
+	return nullptr;
+}
+void incrementCount(string word) {
+	bool isFound = isInTable(word);
+	Word* item = searchTable(word);
+	if (isFound) {
+		++(item->count); // Do we need to increment count here?
+	}
+}
 void printTopN(int n);
 int getNumCollisions();
 int getNumItems();
 int getTotalNumWords();
-wordItem* createNode(string word, wordItem* next);
+
+Word* createNode(string word, Word* next){
+	Word* node = new Word;
+	node->word = word;
+	node->next = next;
+	return node;
+}
 
 
 /* member functions */
-unsigned int getHash(string word){
-	int wordValue = word;
+unsigned int getHash(string word) {
+	unsigned int​ hashValue = ​5381​; 
+    int ​length = word.​length​();
+    
+    for ​(​int​ i=​0​;i<length;i++) {
+        hashValue=((hashValue<<​5​)+hashValue) + word[i]; 
+    }
+    hashValue %= hashTableSize; 
+    return ​hashValue; 
 }
-wordItem* searchTable(string word);
+Word* searchTable(string word);
